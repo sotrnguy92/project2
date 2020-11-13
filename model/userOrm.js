6720dec48b85bf3034f660bd35225bebcfcd3f9d
sotrnguy92 <sonnguyen092@gmail.com>
2020-11-12 22:00:57 -0800

    created a user orm to add a user and return the added user

---
+++ model/userOrm.js
@@ -0,0 +1,43 @@
+const {
+    insertUserQuery,
+    findAllUsers,
+    deleteUserQuery,
+    findUserByIdQuery,
+} = require('./userQueries');
+
+const connection = require('../config/connection');
+
+const fetchUsers = async () => {
+    try {
+        const rows = await connection.query(findAllUsers);
+        console.log(rows);
+        return rows;
+    }catch (e) {
+        throw new Error(e);
+    }
+};
+
+const fetchUserById = async (id)  => {
+    try {
+        const [result] = await connection.query(findUserByIdQuery, id);
+        console.log(result[0]);
+        return result[0];
+    }catch (e) {
+        throw new Error(e);
+    }
+}
+
+const insertUserToDb = async (username, password) => {
+    try {
+        const [result] = await connection.query(insertUserQuery, [username, password]);
+        const [insertedUser] = await connection.query(findUserByIdQuery, result.insertId);
+        console.log(insertedUser[0]);
+        return insertedUser[0];
+    }catch (e) {
+        throw new Error(e);
+    }
+}
+
+insertUserToDb ('jax', 'password');
+
+// fetchUserById(1);
--- model/userQueries.js
+++ model/userQueries.js
@@ -1,3 +1,13 @@
 const insertUserQuery = 'INSERT INTO users (username, password) VALUES (?,?);';
-const deleteUserQuery = 'DELETE FROM users WHERE id ?;';
+const deleteUserQuery = 'DELETE FROM users WHERE id = ?;';
 const findAllUsers = 'SELECT id, username FROM users;';
+const findUserByIdQuery = 'SELECT id, username FROM users WHERE id = ?;';
+
+module.exports= {
+    insertUserQuery,
+    findAllUsers,
+    deleteUserQuery,
+    findUserByIdQuery
+}
+
+
