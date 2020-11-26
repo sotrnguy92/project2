aad3f3cd5735dbea4fb1ac6a786a7e056a1b5c17
sotrnguy92 <sonnguyen092@gmail.com>
2020-11-25 20:43:09 -0800

    change app.get on server index.js

--- index.js
+++ index.js
@@ -9,10 +9,11 @@ const PORT = process.env.PORT || 3001;
 
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
-    app.get("/*", function(req, res) {
-        res.sendFile(path.join(__dirname, "./client/build/index.html"));
+    app.get('*', (request, response) => {
+        response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
     });
 }
+
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(routes);
