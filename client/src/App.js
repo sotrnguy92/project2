070458851802301a1d7dc2dde601ca5bc5756db4
sotrnguy92 <sonnguyen092@gmail.com>
2020-11-25 21:51:28 -0800

    comment back in app.get in server and change router to HashRouter

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,12 @@
 
 import React from 'react';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+import {HashRouter as Router, Route} from 'react-router-dom';
 
 import HeadBar from './components/HeadBar'
 import SignIn from "./components/SignIn";
 import SignUp from "./components/SignUp";
 import TodoView from "./components/TodoView";
-// import {WrappedTodoView} from './components/TodoView'
-// import {WrappedSignIn} from './components/testSignIn'
+
 import {UserListView} from "./components/AllUsers"
 import SingleUserTodoView from "./components/SingleUserTodoView";
 
--- index.js
+++ index.js
@@ -9,6 +9,9 @@ const PORT = process.env.PORT || 3001;
 
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
+    app.get("/*", function(req, res) {
+        res.sendFile(path.join(__dirname, "./client/build/index.html"));
+    });
 }
 
 app.use(express.urlencoded({extended: true}));
